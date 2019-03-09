b20ceddd5208dd604b44c6dae788d2cc6de8bf9d
db1995 <448099205@qq.com>
2019-03-09 13:32:23 +0800

    Add main application class

---
+++ src/main/java/com/github/echat/EchatApplication.java
@@ -0,0 +1,16 @@
+package com.github.echat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author db1995
+ */
+@SpringBootApplication
+public class EchatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EchatApplication.class, args);
+    }
+
+}
